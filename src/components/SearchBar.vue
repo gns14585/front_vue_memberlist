8f56c1ec113b28e654e32d1905d7856c02bb4d76
이정훈 <gns14585@naver.com>
2024-04-23 02:08:54 +0900

    검색기능 구현

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="search-box">
-    <input type="search" v-model="inputText" placeholder="이름 입력" />
-    <button @click="searchMembers">검색</button>
+    <input type="search" v-model="inputText" placeholder="검색어 입력" />
+    <button @click="triggerSearch">검색</button>
   </div>
 </template>
 
 <script setup>
-import { ref, watch } from "vue";
-import Swal from "sweetalert2";
+import { ref } from "vue";
+import _ from "lodash";
+import Swal from "sweetalert2"; // lodash 라이브러리 임포트
 
 const props = defineProps({
   members: Array,
@@ -15,12 +16,26 @@ const props = defineProps({
 
 const inputText = ref("");
 
-watch(inputText, (newValue, oldValue) => {
-  const findMember = props.members.filter((member) => {
-    return member.name.include(newValue);
-  });
-  console.log(findMember);
+// 검색 로직을 디바운스 함수로 감싸기
+const debouncedSearch = _.debounce((newValue) => {
+  const findMember = props.members.filter((member) =>
+    member.name.toLowerCase().includes(newValue.toLowerCase()),
+  );
+  if (findMember.length > 0) {
+    console.log(findMember);
+  } else {
+    Swal.fire({
+      title: "검색 결과 없음",
+      text: "해당 이름을 가진 회원이 없습니다.",
+      icon: "info",
+    });
+  }
 });
+
+// 버튼 클릭시 검색 실행
+function triggerSearch() {
+  debouncedSearch(inputText.value);
+}
 </script>
 
 <style scoped>
